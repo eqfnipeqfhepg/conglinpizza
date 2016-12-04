d1775d6c5121788d71c54496c0b4058955b6dbe6
eqfnipeqfhepg <conglin8888@Gmail.com>
2016-12-04 15:24:55 -0800

    Add files via upload

--- function.js
+++ function.js
@@ -11,6 +11,23 @@ var toppinglist = [
                   {type:'olive',price:3}
                   ];
 
+var addressField = document.getElementById('search_address');
+var geocoder = new google.maps.Geocoder();
+function search() {
+    geocoder.geocode(
+       
+        function(results, status) { 
+            if (status == google.maps.GeocoderStatus.OK) { 
+                var loc = results[0].geometry.location;
+                // use loc.lat(), loc.lng()
+            } 
+            else {
+                alert("Not found: " + status); 
+            } 
+        }
+    );
+};
+
 function showinfo(){
 
    
